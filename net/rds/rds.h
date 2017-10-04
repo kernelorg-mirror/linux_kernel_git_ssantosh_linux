c40f6c3664928f4d64747a582e715551d3e66d0e
Wei Lin Guay <wei.lin.guay@oracle.com>
2017-10-04 11:45:19 +0200

    rds: use multiple sge than buddy allocation in congestion code
    For consistency with data fragment code, use multiple sges for
    congestion code too
    
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    Signed-off-by: Wei Lin Guay <wei.lin.guay@oracle.com>
    Acked-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>

--- net/rds/rds.h
+++ net/rds/rds.h
@@ -59,7 +59,6 @@ void rdsdebug(char *fmt, ...)
 #define RDS_CONG_PAGE_SIZE	BIT(12)
 #define RDS_CONG_MAP_PAGES	(PAGE_ALIGN(RDS_CONG_MAP_BYTES) / RDS_CONG_PAGE_SIZE)
 #define RDS_CONG_MAP_PAGE_BITS	(RDS_CONG_PAGE_SIZE * 8)
-#define RDS_CONG_MAP_SGE	1
 
 struct rds_cong_map {
 	struct rb_node		m_rb_node;
