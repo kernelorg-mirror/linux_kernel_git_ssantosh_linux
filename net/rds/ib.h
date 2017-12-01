f7a462aa7b685cc5440b4d7451f7cb0c4e484b88
Santosh Shilimkar <santosh.shilimkar@oracle.com>
2017-12-01 12:19:22 -0800

    rds: ib: make fragment size dynamic
    An IB HCA transmitting an RC transport message larger than
    the IB MTU, will scatter it up into multiple IB packets.
    The first and mid packets are IB MTU in size, with the last
    packet carrying the remainder. Nevertheless, RDS fragments
    each payload into PAGE_SIZE, which is usually 4KB. Patch makes
    the RDS_FRAG_SIZE dynamic.
    
    In the preperation for subsequent patch(es), this patch adds
    per connection peer negotiation to determine the supported
    fragment size for RDMA transport.
    
    Tested-by: Wei Lin Guay <wei.lin.guay@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    Signed-off-by: Wei Lin Guay <wei.lin.guay@oracle.com>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>

--- net/rds/ib.h
+++ net/rds/ib.h
@@ -30,7 +30,7 @@ extern struct rw_semaphore rds_ib_devices_lock;
 extern struct list_head rds_ib_devices;
 
 /*
- * IB posts RDS_FRAG_SIZE fragments of pages to the receive queues to
+ * IB posts i_frag_sz fragments of pages to the receive queues to
  * try and minimize the amount of memory tied up both the device and
  * socket receive queues.
  */
@@ -180,6 +180,8 @@ struct rds_ib_connection {
 
 	/* Protocol version specific information */
 	unsigned int		i_flowctl:1;	/* enable/disable flow ctl */
+	u16			i_frag_sz;	/* IB fragment size */
+	u8			i_frag_pages;
 
 	/* Batched completions */
 	unsigned int		i_unsignaled_wrs;
--- net/rds/ib_recv.c
+++ net/rds/ib_recv.c
@@ -67,7 +67,7 @@ void rds_ib_recv_init_ring(struct rds_ib_connection *ic)
 
 		sge = &recv->r_sge[1];
 		sge->addr = 0;
-		sge->length = RDS_FRAG_SIZE;
+		sge->length = ic->i_frag_sz;
 		sge->lkey = ic->i_pd->local_dma_lkey;
 	}
 }
@@ -295,7 +295,7 @@ static struct rds_page_frag *rds_ib_refill_one_frag(struct rds_ib_connection *ic
 
 		sg_init_table(&frag->f_sg, 1);
 		ret = rds_page_remainder_alloc(&frag->f_sg,
-					       RDS_FRAG_SIZE, page_mask);
+					       ic->i_frag_sz, page_mask);
 		if (ret) {
 			kmem_cache_free(rds_ib_frag_slab, frag);
 			return NULL;
@@ -530,6 +530,7 @@ static struct list_head *rds_ib_recv_cache_get(struct rds_ib_refill_cache *cache
 
 int rds_ib_inc_copy_to_user(struct rds_incoming *inc, struct iov_iter *to)
 {
+	struct rds_ib_connection *ic = inc->i_conn->c_transport_data;
 	struct rds_ib_incoming *ibinc;
 	struct rds_page_frag *frag;
 	unsigned long to_copy;
@@ -543,13 +544,13 @@ int rds_ib_inc_copy_to_user(struct rds_incoming *inc, struct iov_iter *to)
 	len = be32_to_cpu(inc->i_hdr.h_len);
 
 	while (iov_iter_count(to) && copied < len) {
-		if (frag_off == RDS_FRAG_SIZE) {
+		if (frag_off == ic->i_frag_sz) {
 			frag = list_entry(frag->f_item.next,
 					  struct rds_page_frag, f_item);
 			frag_off = 0;
 		}
 		to_copy = min_t(unsigned long, iov_iter_count(to),
-				RDS_FRAG_SIZE - frag_off);
+				ic->i_frag_sz - frag_off);
 		to_copy = min_t(unsigned long, to_copy, len - copied);
 
 		/* XXX needs + offset for multiple recvs per page */
@@ -776,6 +777,7 @@ u64 rds_ib_piggyb_ack(struct rds_ib_connection *ic)
 static void rds_ib_cong_recv(struct rds_connection *conn,
 			      struct rds_ib_incoming *ibinc)
 {
+	struct rds_ib_connection *ic = conn->c_transport_data;
 	struct rds_cong_map *map;
 	unsigned int map_off;
 	unsigned int map_page;
@@ -803,7 +805,7 @@ static void rds_ib_cong_recv(struct rds_connection *conn,
 		uint64_t *src, *dst;
 		unsigned int k;
 
-		to_copy = min(RDS_FRAG_SIZE - frag_off,
+		to_copy = min(ic->i_frag_sz - frag_off,
 			      RDS_CONG_PAGE_SIZE - map_off);
 		BUG_ON(to_copy & 7); /* Must be 64bit aligned. */
 
@@ -828,7 +830,7 @@ static void rds_ib_cong_recv(struct rds_connection *conn,
 		}
 
 		frag_off += to_copy;
-		if (frag_off == RDS_FRAG_SIZE) {
+		if (frag_off == ic->i_frag_sz) {
 			frag = list_entry(frag->f_item.next,
 					  struct rds_page_frag, f_item);
 			frag_off = 0;
@@ -943,8 +945,8 @@ static void rds_ib_process_recv(struct rds_connection *conn,
 	list_add_tail(&recv->r_frag->f_item, &ibinc->ii_frags);
 	recv->r_frag = NULL;
 
-	if (ic->i_recv_data_rem > RDS_FRAG_SIZE)
-		ic->i_recv_data_rem -= RDS_FRAG_SIZE;
+	if (ic->i_recv_data_rem > ic->i_frag_sz)
+		ic->i_recv_data_rem -= ic->i_frag_sz;
 	else {
 		ic->i_recv_data_rem = 0;
 		ic->i_ibinc = NULL;
